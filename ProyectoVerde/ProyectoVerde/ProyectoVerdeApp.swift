985f3aca933160a993f3cf254744cca0a6ad9048
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-11-10 23:17:01 -0600

    proyecto
    se cargo el proyecto

---
+++ ProyectoVerde/ProyectoVerde/ProyectoVerdeApp.swift
@@ -0,0 +1,29 @@
+//
+//  ProyectoVerdeApp.swift
+//  ProyectoVerde
+//
+//  Created by Oscar Valdes on 08/11/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+@main
+struct ProyectoVerdeApp: App {
+    let persistenceController = PersistenceController.shared
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
